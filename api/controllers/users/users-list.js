31691e16c63c9808abb4e2f5acb3bfefb59447e7
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-24 15:51:30 +0530

    Add files via upload

--- api/controllers/users/users-list.js
+++ api/controllers/users/users-list.js
@@ -2,11 +2,9 @@ const UserService = require("../../services/user.service");
 
 module.exports = async function usersList(req, res) {
     try {
-        let balance = req.body.balance;
-        let userId = req.body.userId;
-        let updatebalance = await UserService.userFullList(balance, userId);
-        res.json(updatebalance);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+        let userLists = await UserService.userFullList();
+        res.json(userLists);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
     }
 }
