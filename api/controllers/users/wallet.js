181d6859314ea696cf9cf6ba78eb8da0b3771306
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-24 18:11:08 +0530

    Add files via upload

--- api/controllers/users/wallet.js
+++ api/controllers/users/wallet.js
@@ -3,7 +3,8 @@ const UserService = require("../../services/user.service");
 
 module.exports = async function wallet(req, res) {
     try {
-        let userId = req.body.loggedInUserId;
+        // let userId = req.body.loggedInUserId;
+        let userId = req.query.userId;
         let walletBals = await UserService.walletBalance(userId);
         res.json(walletBals);
     } catch (error) {
