6badda99389dd99108fe37b5d6396bea22864618
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-26 12:46:54 +0530

    Add files via upload

--- api/dao/product.dao.js
+++ api/dao/product.dao.js
@@ -2,31 +2,29 @@ class ProductDAO {
 
     static async findOne(id) {
         let ds = await sails.getDatastore();
-        const result = await ds.sendNativeQuery("select * from products where id=$1", [id]);
+        const result = await ds.sendNativeQuery('select * from products where id=$1', [id]);
         let data = result.rows;
-        let products = data.length > 0 ? data[0] : null;
-        return products;
-
+        return data.length > 0 ? data[0] : null;
     }
 
     static async getAllProducts() {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select * from products");
+        let result = await ds.sendNativeQuery('select * from products');
         return result.rows;
     }
 
     static async searchProducts(brandName) {
         let ds = await sails.getDatastore();
-        const result = await ds.sendNativeQuery("select * from products where brand_name IN($1)", [brandName]);
+        const result = await ds.sendNativeQuery('select * from products where brand_name IN($1)', [brandName]);
         return result.rows;
     }
 
     static async findActive() {
         let ds = await sails.getDatastore();
-        const result = await ds.sendNativeQuery("select * from products where active =1");
+        const result = await ds.sendNativeQuery('select * from products where active =1');
         return result.rows;
     }
-    
+
 }
 
 
