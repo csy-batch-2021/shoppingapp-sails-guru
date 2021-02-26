1e874122ef690e3207b3a4822855057bc19a0a96
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-26 15:38:56 +0530

    Add files via upload

--- api/dao/userdao.js
+++ api/dao/userdao.js
@@ -1,10 +1,10 @@
 class UserDAO {
 
     static async save(user, hash) {
-        let role = user.role != "" && user.role != null ? user.role : "USER";
+        let role = user.role != "" && user.role != null ? user.role : 'USER';
         let ds = await sails.getDatastore();
         let params = [user.name, user.email, hash, role];
-        let sql = "insert into users (user_name,email,password,role,active) values ($1,$2,$3,$4,1)";
+        let sql = 'insert into users (user_name,email,password,role,active) values ($1,$2,$3,$4,1)';
         let result = await ds.sendNativeQuery(sql, params);
         return result.rows;
 
@@ -12,67 +12,67 @@ class UserDAO {
 
     static async findByEmail(email) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select 1 from users where email=$1", [email]);
+        let result = await ds.sendNativeQuery('select 1 from users where email=$1', [email]);
         let data = result.rows;
         return data[0];
     }
 
     static async createWalletAccount(userId) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("insert into wallet(user_id,balance) values ($1,0)", [userId]);
+        let result = await ds.sendNativeQuery('insert into wallet(user_id,balance) values ($1,0)', [userId]);
         return result.rows;
     }
 
     static async findOne(id) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select * from users where id=$1", [id]);
+        let result = await ds.sendNativeQuery('select * from users where id=$1', [id]);
         let data = result.rows;
         return data[0];
     }
 
     static async findUser(email) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select * from users where email=$1", [email]);
+        let result = await ds.sendNativeQuery('select * from users where email=$1', [email]);
         return result.rows;
     }
 
     static async findWalletUserId(id) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select * from wallet where user_id=$1", [id]);
+        let result = await ds.sendNativeQuery('select * from wallet where user_id=$1', [id]);
         let data = result.rows;
         return data[0];
     }
 
     static async addWalletBalance(bals, id) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("update wallet set balance = balance + $1 where user_id =$2", [bals, id]);
+        let result = await ds.sendNativeQuery('update wallet set balance = balance + $1 where user_id =$2', [bals, id]);
         return result.rows;
     }
 
     static async updatedWalletBalance(bals, id) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("update wallet set balance = $1 where user_id =$2", [bals, id]);
+        let result = await ds.sendNativeQuery('update wallet set balance = $1 where user_id =$2', [bals, id]);
         return result.rows;
     }
 
     static async updatePassword(hash, userId) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("update users set password =$1 where id= $2", [hash, userId]);
+        let result = await ds.sendNativeQuery('update users set password =$1 where id= $2', [hash, userId]);
         return result.affectedRows;
     }
 
     static async userFullList() {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select u.id, user_name, email, role, balance from users u right join wallet w on(u.id = w.user_id)");
+        let result = await ds.sendNativeQuery('select u.id, user_name, email, role, balance from users u right join wallet w on(u.id = w.user_id)');
         return result.rows;
     }
 
     static async transactionList(walletId, amount, transaction_date, comments) {
         let ds = await sails.getDatastore();
-        let transaction_type = "DEBIT";
-        let status = "SUCCESS";
+        let transaction_type = 'DEBIT';
+        let status = 'SUCCESS';
         let params = [walletId, amount, transaction_type, status, transaction_date, comments];
-        let sql = "insert into transactions(account_id,amount,transaction_type,status,transaction_date,comments) values ($1,$2,$3,$4,$5,$6)";
+        let sql = 'insert into transactions(account_id,amount,transaction_type,status,transaction_date,comments) values ($1,$2,$3,$4,$5,$6)';
         let result = await ds.sendNativeQuery(sql, params);
         return result.rows;
     }
@@ -81,10 +81,10 @@ class UserDAO {
     static async refundStatus(transactionList) {
         let ds = await sails.getDatastore();
         let transaction_date = new Date();
-        let transaction_type = "CREDIT";
-        let comments = "Refunded # " + transactionList.id;
+        let transaction_type = 'CREDIT';
+        let comments = 'Refunded # ' + transactionList.id;
         let params = [transactionList.account_id, transactionList.amount, transaction_type, transactionList.status, transaction_date, comments];
-        let sql = "insert into transactions(account_id,amount,transaction_type,status,transaction_date,comments) values ($1,$2,$3,$4,$5,$6)";
+        let sql = 'insert into transactions(account_id,amount,transaction_type,status,transaction_date,comments) values ($1,$2,$3,$4,$5,$6)';
         let result = await ds.sendNativeQuery(sql, params);
         return result.rows;
     }
@@ -92,10 +92,10 @@ class UserDAO {
     static async updateUser(userId, name, email) {
         try {
             let ds = await sails.getDatastore();
-            let result = await ds.sendNativeQuery("update users set user_name =$1, email =$2 where id =$3", [name, email, userId]);
+            let result = await ds.sendNativeQuery('update users set user_name =$1, email =$2 where id =$3', [name, email, userId]);
             return result.rows;
         } catch (error) {
-            throw new Error("Mail Already exists")
+            throw new Error('Mail Already exists')
         }
 
     }
@@ -109,7 +109,7 @@ class UserDAO {
 
     static async myTransactions(userId) {
         let ds = await sails.getDatastore();
-        let result = await ds.sendNativeQuery("select * from transactions where account_id = (select id from wallet where user_id =$1)", [userId]);
+        let result = await ds.sendNativeQuery('select * from transactions where account_id = (select id from wallet where user_id =$1)', [userId]);
         return result.rows;
     }
 }
