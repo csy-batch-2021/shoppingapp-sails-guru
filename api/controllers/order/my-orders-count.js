12bde4c87fbbfcc1f578658eeea66dee3dc3030b
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-26 10:50:54 +0530

    Add files via upload

--- api/controllers/order/my-orders-count.js
+++ api/controllers/order/my-orders-count.js
@@ -5,15 +5,10 @@ module.exports = async function myOrdersCount(req, res) {
     let userId = req.query.userId;
     try {
         UserValidator.toCheckValidUserId(userId)
-        // console.log("userId try block");
         let myOrders = await OrderService.myOrderStatusCount(userId);
-        // const myOrdersList = myOrders.sort(
-        //   (a, b) => b.created_date - a.created_date
-        // );
-        // console.table(myOrders);
         res.json(myOrders);
     } catch (err) {
-        // console.log(err.message);
+        console.log(err.message);
         res.status(404).json({ message: err.message });
     }
 }
