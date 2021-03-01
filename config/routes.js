b86323b2baa8da349726fc737f4152274f652fc9
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-03-01 14:34:54 +0530

    Add files via upload

--- config/routes.js
+++ config/routes.js
@@ -36,28 +36,88 @@ module.exports.routes = {
 
 
   //REST API
-  "post /api/login": { action: "users/login" },
-  "post /api/register": { action: "users/register" },
-  "get /api/users": { action: "users/login" },
-  "get /api/userLists": { action: "users/users-list" },
-  "patch /api/addBalance": { action: "users/update-wallet" },
-  "get /api/walletBalance": { action: "users/wallet" },
-  "patch /api/changeUserPassword": { action: "users/change-password" },
-  "patch /api/updateProfile": { action: "users/edit-profile" },
-  "get /api/transactions": { action: "users/transaction-list" },
-  "patch /api/myTransactions": { action: "users/mytransaction-list" },
+
+  /**
+   * This API is used to Get All Users list from Users table. 
+  */
+  "GET /api/userLists": { action: "users/users-list" },
+
+  /**
+   * This API is used to Get All Wallet Balance from Wallet table.
+  */
+  "GET /api/walletBalance": { action: "users/wallet" },
+
+  /**
+  * This API is used to List All User Transaction from Transaction table by Grouping function.
+ */
+  "GET /api/transactions": { action: "users/transaction-list" },
+
+  /**
+   * This API is used to List All Products from Product table.
+  */
   "GET /api/products": { action: "product/list" },
-  "POST /api/addOrder": { action: "order/add-order" },
-  "GET /api/orders": { action: "order/all-order" },
-  "POST /api/changeorderstatus": { action: "order/change-order-status" },
-  "PATCH /api/order/:id/cancel": { action: "order/cancel-order" },
-  "GET /api/product/search": { action: "product/search-product" },
-  "GET /api/getAllProducts": { action: "product/get-all-products" },
+
+  /**
+    * This API is used to List One User Orders from Orders table.
+   */
   "GET /api/myOrders": { action: "order/my-orders" },
+
+  /**
+   * This API is used to Get The Counts of the Orders (DELIVERY/CANCEL).
+  */
   "GET /api/orderStatusCount": { action: "order/my-orders-count" },
-  "POST /api/product/addRating": { action: "product/add-product-rating" },
+
+  /**
+   *  This API is used to Register as new User and Admin.
+  */
+  "POST /api/register": { action: "users/register" },
+
+  /**
+   *  This API is used to Login as a User and Admin.
+  */
+  "POST /api/login": { action: "users/login" },
+
+  /**
+   * This API is used to List All Orders from Orders table.
+  */
+  "POST /api/addOrder": { action: "order/add-order" },
+
+  /**
+   * This API is used to Change The Order Status (DELIVERY/CANCEL). 
+  */
+  "POST /api/changeorderstatus": { action: "order/change-order-status" },
+
+  /**
+   * This API is used to Change The User Password.
+  */
+  "PATCH /api/changeUserPassword": { action: "users/change-password" },
+
+  /**
+   * This API is used to Add Balance in Wallet. 
+  */
+  "PATCH /api/addBalance": { action: "users/update-wallet" },
+
+  /**
+   * This API is used to Update The User Profile Name and Email.
+  */
+  "PATCH /api/updateProfile": { action: "users/edit-profile" },
+
+  /**
+   * This API is used to List One User Transaction List from Tansaction table and Order table.
+  */
+  "PATCH /api/myTransactions": { action: "users/mytransaction-list" },
+
+  /**
+   * This API is used to Cancel a Order.
+  */
+  "PATCH /api/order/:id/cancel": { action: "order/cancel-order" },
 
 
+  // "GET /api/product/search": { action: "product/search-product" },
+  // "GET /api/orders": { action: "order/all-order" },
+  // "GET /api/getAllProducts": { action: "product/get-all-products" },
+  // "POST /api/product/addRating": { action: "product/add-product-rating" },
+  // "get /api/users": { action: "users/login" },
 
 
 
